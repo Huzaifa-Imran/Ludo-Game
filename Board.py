df1be3f91b31a5139df75bbf7fe62d54e70daadc
Huzaifa Imran <huzaifadsu@gmail.com>
2020-12-25 00:02:34 +0500

    Update Board.py

--- Board.py
+++ Board.py
@@ -2,32 +2,33 @@ import pygame
 
 whiteColor = (255, 255, 255)
 yellowColor = (255, 255, 0)
-greenColor = (0, 255, 0)
-blueColor = (0, 0, 255)
-redColor = (255, 0, 0)
+greenColor = (102, 255, 51)
+blueColor = (0, 153, 255)
+redColor = (255, 51, 51)
 blackColor = (0, 0, 0)
 grayColor = (127, 127, 127)
 
-endingPoint = [51, 12, 38, 25]
 Tokens = []
-# Tokens = [[1,4,8,-1], [37,48,6,18], [-1,-1,-1,-1], [9,50,43,20]]
 tokenHoused = [[False, False, False, False], [False, False, False, False], [False, False, False, False], [False, False, False, False]]
-colours = [blueColor, yellowColor, redColor, greenColor]
+colours = [blueColor, yellowColor, greenColor, redColor]
+tokenColours = [(0, 0, 204), (255, 204, 0), (0, 179, 0), (204, 0, 0)]
 playerHasKilled = [False, False, False, False]
 blocks = [[], [], [], []]
 
 # location of slots in which tokens cant move until six is rolled
-jailSlots = [[(1.5, 1.5), (3.5, 1.5), (1.5, 3.5), (3.5, 3.5)], [(10.5, 1.5), (12.5, 1.5), (10.5, 3.5), (12.5, 3.5)], [(1.5, 10.5), (3.5, 10.5), (1.5, 12.5), (3.5, 12.5)], [(10.5, 10.5), (12.5, 10.5), (10.5, 12.5), (12.5, 12.5)]]
+jailSlots = [[(1.5, 1.5), (3.5, 1.5), (1.5, 3.5), (3.5, 3.5)], [(10.5, 1.5), (12.5, 1.5), (10.5, 3.5), (12.5, 3.5)], [(10.5, 10.5), (12.5, 10.5), (10.5, 12.5), (12.5, 12.5)],  [(1.5, 10.5), (3.5, 10.5), (1.5, 12.5), (3.5, 12.5)]]
 # location of slots which are general and can be moved onto by any token
 commonSlots = [(0, 6), (1, 6), (2, 6), (3, 6), (4, 6), (5, 6), (6, 5), (6, 4), (6, 3), (6, 2), (6, 1), (6, 0), (7, 0), (8, 0), (8, 1), (8, 2), (8, 3), (8, 4),
                 (8, 5), (9, 6), (10, 6), (11, 6), (12, 6), (13, 6), (14, 6), (14, 7), (14, 8), (13, 8), (12, 8), (11, 8), (10, 8), (9, 8), (8, 9), (8, 10), (8, 11),
                 (8, 12), (8, 13), (8, 14), (7, 14), (6, 14), (6, 13), (6, 12), (6, 11), (6, 10), (6, 9), (5, 8), (4, 8), (3, 8), (2, 8), (1, 8), (0, 8), (0, 7)]
 # location of slots which are specific to each house
 houseSlots = [[(1, 7), (2, 7), (3, 7), (4, 7), (5, 7)], [(7, 1), (7, 2), (7, 3), (7, 4), (7, 5)],
-                [(7, 13), (7, 12), (7, 11), (7, 10), (7, 9)],
-                [(13, 7), (12, 7), (11, 7), (10, 7), (9, 7)]]
+                [(13, 7), (12, 7), (11, 7), (10, 7), (9, 7)], [(7, 13), (7, 12), (7, 11), (7, 10), (7, 9)]]
 # index of commonSlots on which tokens spawn from jail
-spawnPos = [1, 14, 40, 27]
+spawnPos = [1, 14, 27, 40]
+# index of common slots on which tokens end their route and move inside houseSlots
+endingSlots = [51, 12, 25, 38]
+
 
 def setWindow():
     global screen, boardX, boardY, smallSquare
@@ -44,7 +45,6 @@ def updateScreen():
     screen.fill(whiteColor)
     createBoard(boardX, boardY, smallSquare)
     drawTokens(boardX, boardY, smallSquare)
-    pygame.display.flip()
     pygame.display.update()
     pygame.event.clear()
 
@@ -53,6 +53,8 @@ def createBoard(x, y, smallSquare):
     for colour in range(4):
         xAxis, yAxis = commonSlots[spawnPos[colour]]
         pygame.draw.rect(screen, colours[colour], [x + xAxis*smallSquare, y + yAxis*smallSquare, smallSquare, smallSquare], 0)
+        xAxis, yAxis = commonSlots[endingSlots[colour]-3]
+        pygame.draw.rect(screen, grayColor, [x + xAxis*smallSquare, y + yAxis*smallSquare, smallSquare, smallSquare], 0)
         if not playerHasKilled[colour]:
             houseSlotColor = grayColor
         else:
@@ -89,28 +91,27 @@ def createBoard(x, y, smallSquare):
     pygame.draw.rect(screen, greenColor, [x + smallSquare*9, y + smallSquare*9, smallSquare*6, smallSquare*6], 0)
     pygame.draw.polygon(screen, greenColor, [[x + smallSquare*9, y + smallSquare*6], [x + smallSquare*9, y + smallSquare*9], [x + smallSquare*7.5, y + smallSquare*7.5]], 0)
     pygame.draw.rect(screen, whiteColor, [x + smallSquare*10, y + smallSquare*10, smallSquare*4, smallSquare*4], 0)
-    
-    return screen
+
 
 def drawTokens(x, y, smallSquare):
     for colour in range(4):
         for number in range(4):
             pos = Tokens[colour][number]
-            if pos == -2:
+            if pos == -2: # if token has won
                 continue
-            elif pos == -1:
+            elif pos == -1: # if token is in jail
                 i, j = jailSlots[colour][number]
-            elif tokenHoused[colour][number]:
-                print(tokenHoused)
-                print(pos)
-                print(colour)
+            elif tokenHoused[colour][number]: # if token is in house slots
                 i, j = houseSlots[colour][pos]
-                print(i, j)
-            else:
+            else: # if token is in common slots
                 i, j = commonSlots[pos]
-            pygame.draw.circle(screen, colours[colour], (x + (i+0.5)*smallSquare, y + (j+0.5)*smallSquare), smallSquare/2) 
-            
-    return screen
+            pygame.draw.circle(screen, tokenColours[colour], (x + (i+0.5)*smallSquare, y + (j+0.5)*smallSquare), smallSquare/3) # draw a small circle at position of token
+
+    for colour in range(4):
+        for pos in blocks[colour]: # if a block exists
+            i, j = commonSlots[pos]
+            pygame.draw.circle(screen, tokenColours[colour], (x + (i+0.5)*smallSquare, y + (j+0.5)*smallSquare), smallSquare/2) # draw a big circle at position of block
+
 def moveToken(colour, number, steps):
     pos = Tokens[colour][number]
     if pos == -1:
@@ -118,9 +119,9 @@ def moveToken(colour, number, steps):
         Tokens[colour][number] = spawnPos[colour]
     elif tokenHoused[colour][number] and pos + steps == 5:
         Tokens[colour][number] = -2
-    else: 
-        if pos <= endingPoint[colour] and pos+steps > endingPoint[colour] and playerHasKilled[colour]:
-            newPos = (pos + steps) % (endingPoint[colour] + 1)
+    else:
+        if pos <= endingSlots[colour] and pos+steps > endingSlots[colour] and playerHasKilled[colour]:
+            newPos = (pos + steps) % (endingSlots[colour] + 1)
             if newPos == 5:  # token has won
                 newPos = -2
             tokenHoused[colour][number] = True
@@ -136,6 +137,8 @@ def moveToken(colour, number, steps):
 def tokensClash(colour, number, newPos, pos): #check if there is already a token at current or new position
             for i in range(4):
                 for j in range(4):
+                    if tokenHoused[i][j] != tokenHoused[colour][number]:
+                        continue
                     if pos != -1 and pos == Tokens[i][j] and number != j: # if old position is not jail and another token exists at old position means it was previously in a block
                         blocks[colour].remove(pos)
                     if newPos == Tokens[i][j]:  # token already exists at new position
