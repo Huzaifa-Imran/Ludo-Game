bafe5ebe938e8a9c93514700e1b0165863bac2f5
Huzaifa Imran <huzaifadsu@gmail.com>
2020-12-24 19:00:09 +0500

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,36 +1,27 @@
-import pygame
 import Board
 import random
+import copy as cp
 
 currentColour = 0
-currentPlayer = -1
 playerNames = []
 playerColours = []
-
+colourOrder = ['blue', 'yellow', 'green', 'red']
+sixesRolled = 0
 
 def gameSetup():
-    global boardX, boardY, smallSquare, playerColours, playerNames, screen, background
-    pygame.init()
-    smallSquare = 60
-    width = 1000
-    height = 1000
-    screen = pygame.display.set_mode((width, height), 0, 32)
-    pygame.display.set_caption('Ludo')
-    background = pygame.Surface(screen.get_size())
-    background = background.convert()
-    background.fill(Board.whiteColor)
-    boardX = (width - smallSquare * 15) / 2
-    boardY = (height - smallSquare * 15) / 2
+    Board.setWindow()
     for i in range(4):
         Board.Tokens.append([])
         for j in range(4):
             Board.Tokens[i].append(-1)  # put all tokens in jail at the beginning i.e -1
-    drawGraphics()
+    Board.updateScreen()
     answer = int(input("How many players?(2-4): "))
-    if answer >= 2 and answer <= 4:
-        players = answer
-    else:
-        players = int(input("Please enter a number between 2 and 4"))
+    while(True):
+        if answer >= 2 and answer <= 4:
+            players = answer
+            break
+        else:
+            answer = int(input("Please enter a number between 2 and 4"))
 
     print("Enter name of players.\n")
     for i in range(players):
@@ -38,33 +29,56 @@ def gameSetup():
         playerNames.append(answer)
 
     availableColours = [0, 1, 2, 3]
-    print("Choose colours.\nBlue = 0\nYellow = 1\nRed = 2\nGreen = 3")
-    for i in range(players):
-        answer = int(input(f"Colour for {playerNames[i]}: "))
-        if answer in availableColours:
-            availableColours.remove(answer)
-        else:
-            print("That colour is already selected or doesn't exists. Choosing first available colour for you.")
-            answer = availableColours.pop(0)
-        playerColours.append(answer)
-
+    answer = input("Do you want to choose colours yourself?('yes' or 'no'): ")
+    if answer == "yes":
+        print("Blue = 0\nYellow = 1\nGreen = 2\nRed = 3")
+        for i in range(players):
+            if len(availableColours) > 1:
+                answer = int(input(f"Colour for {playerNames[i]}{availableColours}: "))
+                if answer in availableColours:
+                    availableColours.remove(answer)
+                else:
+                    answer = availableColours.pop(0)
+                    print(f"That colour is already selected or doesn't exists. Assigning you colour {colourOrder[answer]}")
+            else:
+                answer = availableColours[0]
+            playerColours.append(answer)
+    else:
+        random.shuffle(availableColours)
+        for i in range(players):
+            playerColours.append(availableColours[i])
     nextTurn()
 
 
 def play():
     gameSetup()
     while len(playerColours) >= 2:
-        input("Press enter to roll dice.")
+        input(f"\nCurrent Turn: {playerNames[currentPlayer]}({colourOrder[currentPlayer]}) Press enter to roll dice.")
         rollDice()
-        drawGraphics()
+        Board.updateScreen()
 
 
 def rollDice():
+    global sixesRolled, tempState
     roll = random.randint(1, 6)
-    print(playerNames[currentPlayer], " rolled ", str(roll))
-    moveableTokens(playerColours[currentPlayer], roll)
-    nextTurn()
+    print(f"{playerNames[currentPlayer]} rolled {roll}")
+    if roll == 6:
+        sixesRolled += 1
+        if sixesRolled != 3:
+            if sixesRolled == 1:
+                tempState = (cp.copy(Board.Tokens), cp.copy(Board.playerHasKilled[currentPlayer]), cp.copy(Board.tokenHoused[currentPlayer]), cp.copy(Board.blocks[currentPlayer]))
+            moveableTokens(playerColours[currentPlayer], roll)
+        else:
+            Board.Tokens, Board.playerHasKilled[currentPlayer], Board.tokenHoused[currentPlayer], Board.blocks[currentPlayer] = tempState
+            print(f"{playerNames[currentPlayer]} rolled three cosecutive sixes! Cancelling previous moves.")
+            sixesRolled = 0
+            
+    else:
+        sixesRolled = 0
+        moveableTokens(playerColours[currentPlayer], roll)
 
+    if sixesRolled == 0:
+        nextTurn()
 
 def moveableTokens(colour, roll):
     possibleTokens = []
@@ -72,21 +86,26 @@ def moveableTokens(colour, roll):
         if canBeMoved(colour, i, roll):
             possibleTokens.append(i)
 
-    if len(possibleTokens) >= 2:  # if there are atleast 2 moveable tokens
-        try:
-            answer = int(input(f"Select a token to move: {possibleTokens}"))
-        except ValueError:
-            answer = 1
-            print(f"Wrong token chosen. Moving token {possibleTokens[answer]}")
+    if len(possibleTokens) >= 1:  # if there are atleast 2 moveable tokens
+        if len(possibleTokens) >= 2:
+            while(True):
+                try:
+                    answer = int(input(f"Select a token to move{possibleTokens}: "))
+                    if answer in possibleTokens:
+                        break
+                    else:
+                        print("That token is not available")
+                except ValueError:
+                    print(f"Invalid input.")
+        else:
+            answer = 0
+            won = Board.moveToken(colour, possibleTokens[0], roll)
         won = Board.moveToken(colour, possibleTokens[answer], roll)
-        print(playerNames[currentPlayer] + " has moved token " + str(possibleTokens[answer]))
+        print(f"{playerNames[currentPlayer]} has moved token {possibleTokens[answer]}")
         if won:
-            print(playerNames[colour], " has won the game!")  # above loop doesnt returns false
+            print(f"{playerNames[colour]} has won the game!")  # above loop doesnt returns false
             playerNames.pop(currentPlayer)
             playerColours.pop(currentPlayer)
-    elif len(possibleTokens) == 1:
-        Board.moveToken(colour, possibleTokens[0], roll)
-        print(playerNames[currentPlayer] + " has moved token " + str(possibleTokens[0]))
 
 
 def canBeMoved(colour, num, roll):
@@ -124,9 +143,3 @@ def nextTurn():
     if currentPlayer == -1:  # if no player has current colour
         nextTurn()  # recursively call next turn which will incremement currentColour until playerColour matches
 
-
-def drawGraphics():
-    screen.blit(background, (0, 0))
-    Board.createBoard(screen, boardX, boardY, smallSquare)
-    Board.drawTokens(screen, boardX, boardY, smallSquare)
-    pygame.display.flip()
