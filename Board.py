4676b00ab342c4b6fafe514b460ee7111c8aee2d
Huzaifa Imran <huzaifadsu@gmail.com>
2020-12-29 23:41:26 +0500

    Update Board.py

--- Board.py
+++ Board.py
@@ -1,4 +1,6 @@
 import pygame
+import pygame.freetype
+import random
 
 whiteColor = (255, 255, 255)
 yellowColor = (255, 255, 0)
@@ -32,17 +34,20 @@ endingSlots = [51, 12, 25, 38]
 
 
 def setWindow():
-    global screen, boardX, boardY, smallSquare
+    global screen, boardX, boardY, smallSquare, rollButtonSurface
     pygame.init()
-    width = 1000
-    height = 1000
     smallSquare = 60
+    width = smallSquare*15+100
+    height = smallSquare*15+100
     boardX = (width - smallSquare * 15) / 2
     boardY = (height - smallSquare * 15) / 2
     screen = pygame.display.set_mode((width, height))
     pygame.display.set_caption('Ludo')
+    # myFont = pygame.font.SysFont('',96)
+    # rollButtonSurface = myFont.render('Roll Dice', False, blackColor)
 
 def updateScreen():
+    pygame.event.clear()
     screen.fill(whiteColor)
     createBoard(boardX, boardY, smallSquare)
     drawTokens(boardX, boardY, smallSquare)
@@ -93,6 +98,11 @@ def createBoard(x, y, smallSquare):
     pygame.draw.polygon(screen, greenColor, [[x + smallSquare*9, y + smallSquare*6], [x + smallSquare*9, y + smallSquare*9], [x + smallSquare*7.5, y + smallSquare*7.5]], 0)
     pygame.draw.rect(screen, whiteColor, [x + smallSquare*10, y + smallSquare*10, smallSquare*4, smallSquare*4], 0)
 
+    # draw roll dice button
+
+    # pygame.draw.rect(screen, grayColor, [x + smallSquare*5, y - 125, smallSquare*5, 100], 0)
+    # screen.blit(rollButtonSurface, (x + smallSquare*5+5, y - 105))
+    # pygame.display.flip()
 
 def drawTokens(x, y, smallSquare):
     for colour in range(4):
@@ -111,7 +121,29 @@ def drawTokens(x, y, smallSquare):
     for colour in range(4):
         for pos in blocks[colour]: # if a block exists
             i, j = commonSlots[pos]
+            blockCount = blocks[colour].count(pos)
             pygame.draw.circle(screen, tokenColours[colour], (x + (i+0.5)*smallSquare, y + (j+0.5)*smallSquare), smallSquare/2) # draw a big circle at position of block
+
+def canBeMoved(colour, num, roll):
+    pos = Tokens[colour][num]
+    if pos == -2:  # if token has finished its run
+        return False  # cant move
+    elif pos == -1:  # if token is in jail
+        if roll == 6:
+            return True  # can be spawned
+        else:
+            return False
+    elif tokenHoused[colour][num]:  # if token is inside houseSlots
+        if pos + roll <= 5:
+            return True
+        else:
+            return False
+    else:
+        for steps in range(1, roll+1):
+            for col in range(4):
+                if col != colour and (pos + steps) % 52 in blocks[col]:
+                    return False
+    return True  # can move in all other scenarios
 
 def moveToken(colour, number, steps):
     pos = Tokens[colour][number]
@@ -123,28 +155,61 @@ def moveToken(colour, number, steps):
     else:
         if pos <= endingSlots[colour] and pos+steps > endingSlots[colour] and playerHasKilled[colour]:
             newPos = (pos + steps) % (endingSlots[colour] + 1)
+            tokenHoused[colour][number] = True
             if newPos == 5:  # token has won
                 newPos = -2
-            tokenHoused[colour][number] = True
         else:
             newPos = (pos + steps) % 52
             tokensClash(colour, number, newPos, pos)
         Tokens[colour][number] = newPos
     for i in range(4):
-        if Tokens[colour][number] != -2:  # if all tokens of a colour have not won
+        if Tokens[colour][i] != -2:  # if all tokens of a colour have not won
             return False  # player has not won
     return True  # player has won
 
 def tokensClash(colour, number, newPos, pos): #check if there is already a token at current or new position
-            for i in range(4):
-                for j in range(4):
-                    if tokenHoused[i][j] != tokenHoused[colour][number]:
-                        continue
-                    if pos != -1 and pos == Tokens[i][j] and number != j: # if old position is not jail and another token exists at old position means it was previously in a block
-                        blocks[colour].remove(pos)
-                    if newPos == Tokens[i][j]:  # token already exists at new position
-                        if colour == i:  # if the token which already exists is of same colour and we have not previously removed a block
-                            blocks[colour].append(newPos)
-                        else:  # if the token which already exists is an enemy
-                            Tokens[i][j] = -1  # put it in jail
-                            playerHasKilled[colour] = True
+    if not tokenHoused[colour][number]:
+        if pos in blocks[colour]:
+            blocks[colour].remove(pos)
+            print(blocks)
+        for i in range(4):
+            for j in range(4):
+                if newPos == Tokens[i][j] and not tokenHoused[i][j]:  # token already exists at new position
+                    if colour == i:
+                        blocks[colour].append(newPos)
+                        print(blocks)
+                        return
+                    else:
+                        Tokens[i][j] = -1  # put it in jail
+                        playerHasKilled[colour] = True
+
+
+def drawDice():
+    size = smallSquare*2            # Size of window/dice
+    spsz = size/10                  # size of spots
+    mx = boardX + smallSquare*7.5             # mid-point of dice (or die?)
+    my = boardY + smallSquare*7.5
+    l = boardX + smallSquare*7
+    t = boardY + smallSquare*7             # location of left and top spots
+    r = boardX + smallSquare*8
+    b = boardY + smallSquare*8              # location of right and bottom spots
+    rolling = 12                    # times that dice rolls before stopping
+    n = int()
+    for i in range(rolling):        # roll the die...
+        n=random.randint(1,6)       # random number between 1 &amp;amp; 6
+        pygame.draw.rect(screen, whiteColor, [boardX + smallSquare*6.5, boardY + smallSquare*6.5, size, size], 0)
+        if n % 2 == 1:
+            pygame.draw.circle(screen,blackColor,(mx,my),spsz)  # middle spot
+        if n==2 or n==3 or n==4 or n==5 or n==6:
+            pygame.draw.circle(screen,blackColor,(l,b),spsz)  # left bottom
+            pygame.draw.circle(screen,blackColor,(r,t),spsz)  # right top
+        if n==4 or n==5 or n==6:
+            pygame.draw.circle(screen,blackColor,(l,t),spsz)  # left top
+            pygame.draw.circle(screen,blackColor,(r,b),spsz)  # right bottom
+        if n==6:
+            pygame.draw.circle(screen,blackColor,(mx,b),spsz)  # middle bottom
+            pygame.draw.circle(screen,blackColor,(mx,t),spsz)  # middle top
+    
+        pygame.display.update()
+        pygame.time.delay(400)
+    return n
